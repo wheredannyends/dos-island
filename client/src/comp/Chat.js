1f7fb8f61562ca83041b69aba197ad14a3c2b3c4
Danny Burton <dannyburton@Dannys-MBP.hitronhub.home>
2018-01-29 18:02:29 -0800

    Added chat comp front-end

--- client/src/comp/Chat.js
+++ client/src/comp/Chat.js
@@ -6,7 +6,8 @@ class App extends Component {
 		super(props);
 		
 		this.state = {
-			
+			chatMode: 0,
+			userName: ""
 		}
 	}
 	
@@ -14,10 +15,68 @@ class App extends Component {
 		this.socket = mySocket("http://localhost:10001");
 	}
 	
+	userNameUpdate = (e) => {
+		this.setState({
+			userName: e.target.value
+		});
+	}
+	
+	joinChat = () => {
+		if (this.state.userName != "") {
+			this.setState({
+				chatMode: 1
+			});
+		} else {
+			alert("Enter your name");
+		}
+	}
+	
 	render() {
+		let chatComp = null;
+		
+		if (this.state.chatMode === 0) {
+			chatComp = (
+				<div className="chat-login flex">
+					<h3>Join the Chat</h3>
+					<input className="input" placeholder="Enter your name" maxlength="10" onChange={this.userNameUpdate} />
+					<button className="button" onClick={this.joinChat}>Enter</button>
+				</div>
+			);
+		} else if (this.state.chatMode === 1) {
+			chatComp = (
+				<div className="flex">
+					<div className="chat-content chat-users">
+						<div className="flex">
+							<h4>Connected Users</h4>
+							<div className="chat-users-list">
+								<div className="flex">
+									<article className="chat-users-thumb flex">
+										D
+									</article>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="chat-content chat-messages">
+						<div className="flex">
+							<article className="chat-bubble">Hello</article>
+							<article className="chat-bubble">This is a test message</article>
+							<article className="chat-bubble">Penis lol wtf</article>
+						</div>
+					</div>
+					<div className="chat-content chat-input">
+						<div className="flex">
+							<input className="input" placeholder="Type a message" />
+							<button className="button">Send</button>
+						</div>
+					</div>
+				</div>
+			);
+		}
+		
 		return (
 			<section className="chat">
-				
+				{chatComp}
 			</section>
 		);
 	}
