574f960adc1f5f948c7cb8a503d628ba99c45e03
wheredannyends <wheredannyends@gmail.com>
2018-01-31 17:35:45 -0800

    Week 2 added

--- client/src/comp/Chat.js
+++ client/src/comp/Chat.js
@@ -7,7 +7,9 @@ class App extends Component {
 		
 		this.state = {
 			chatMode: 0,
-			userName: ""
+			userName: "",
+			users: [],
+			allChats: [],
 		}
 	}
 	
@@ -20,31 +22,79 @@ class App extends Component {
 			userName: e.target.value
 		});
 	}
-
-	
 	
 	joinChat = () => {
 		if (this.state.userName != "") {
 			this.setState({
-				chatMode: 1
+				chatMode: 1,
+			});
+			this.socket.emit("username", this.state.userName);
+
+			this.socket.on("usersjoined", (data) => {
+				this.setState({
+					users: data,
+				});
+			});
+
+			this.socket.on("msgsent", (data) => {
+				this.setState({
+					allChats: data,
+				});
 			});
 		} else {
 			alert("Enter your name");
 		}
 	}
 	
+	sendMsg = () => {
+		var msg = this.state.userName + ": " + this.state.myMsg;
+		
+		this.socket.emit("sendMsg", msg);
+	}
+	
+	handleUserName = (evt) => {
+		this.setState({
+			userName: evt.target.value,
+		})
+	}
+	
+	handleMyMsg = (evt) => {
+		this.setState({
+			myMsg: evt.target.value,
+		})
+	}
+	
 	render() {
 		let chatComp = null;
+		
+		var curUsers = this.state.users.map((obj, i) => {
+			var firstLetter = obj.charAt(0).toUpperCase();
+			console.log("username: "+obj);
+			
+			return (
+				<article key={i} className="chat-users-thumb flex">
+					{firstLetter}
+				</article>
+			);
+		});
 		
 		if (this.state.chatMode === 0) {
 			chatComp = (
 				<div className="chat-login flex">
 					<h3>Join the Chat</h3>
-					<input className="input" placeholder="Enter your name" maxlength="10" onChange={this.userNameUpdate} />
+					<input className="input" placeholder="Enter your name" maxLength="10" onChange={this.handleUserName} />
 					<button className="button" onClick={this.joinChat}>Enter</button>
 				</div>
 			);
 		} else if (this.state.chatMode === 1) {
+			var allChats = this.state.allChats.map((obj, i) => {
+				return (
+					<article key={i} className="chat-bubble">
+						{obj}
+					</article>
+				);
+			});
+			
 			chatComp = (
 				<div className="flex">
 					<div className="chat-content chat-users">
@@ -52,24 +102,20 @@ class App extends Component {
 							<h4>Connected Users</h4>
 							<div className="chat-users-list">
 								<div className="flex">
-									<article className="chat-users-thumb flex">
-										D
-									</article>
+									{curUsers}
 								</div>
 							</div>
 						</div>
 					</div>
 					<div className="chat-content chat-messages">
 						<div className="flex">
-							<article className="chat-bubble">Hello</article>
-							<article className="chat-bubble">This is a test message</article>
-							<article className="chat-bubble">Penis lol wtf</article>
+							{allChats}
 						</div>
 					</div>
 					<div className="chat-content chat-input">
 						<div className="flex">
-							<input className="input" placeholder="Type a message" />
-							<button className="button">Send</button>
+							<input className="input" type="text" onChange={this.handleMyMsg} placeholder="Type a message" />
+							<button className="button" onClick={this.sendMsg}>Send</button>
 						</div>
 					</div>
 				</div>
